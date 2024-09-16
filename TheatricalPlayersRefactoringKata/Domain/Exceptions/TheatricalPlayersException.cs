ec12884d0af01a1170f09d30aab68bef3fc2ac8d
Posccis <lkzq33@gmail.com>
2024-09-16 13:28:21 -0300

    new(teste/victor-hugo): classe de exception para lidar com os erros e retornos

---
+++ TheatricalPlayersRefactoringKata/Domain/Exceptions/TheatricalPlayersException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheatricalPlayersRefactoringKata.Domain.Exceptions
+{
+    public class TheatricalPlayersException : Exception
+    {
+        public int codigo;
+
+        public TheatricalPlayersException(string message, int codigo = 0) : base(message)
+        {
+            codigo = codigo;
+        }
+    }
+}
