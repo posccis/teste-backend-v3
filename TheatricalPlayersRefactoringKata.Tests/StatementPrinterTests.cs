e6c3ef5c506174d693881627a0dc795a07402238
Posccis <lkzq33@gmail.com>
2024-09-16 14:10:34 -0300

    update(teste/victor-hugo): Alteração da pasta da classe StatementPrinter

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using ApprovalTests;
 using ApprovalTests.Reporters;
+using TheatricalPlayersRefactoringKata.Application.Services;
 using TheatricalPlayersRefactoringKata.Domain.Domains;
 using Xunit;
 
