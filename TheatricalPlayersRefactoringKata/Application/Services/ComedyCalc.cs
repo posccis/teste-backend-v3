ec28e334f75c3349f4750d7997c40fa72ff7bf62
Posccis <lkzq33@gmail.com>
2024-09-16 14:09:49 -0300

    new(teste/victor-hugo): criação das classes de calculo para cada genero

---
+++ TheatricalPlayersRefactoringKata/Application/Services/ComedyCalc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheatricalPlayersRefactoringKata.Application.Interfaces;
+using TheatricalPlayersRefactoringKata.Domain.Domains;
+
+namespace TheatricalPlayersRefactoringKata.Application.Services
+{
+    public class ComedyCalc : ITypeCalc
+    {
+        public string CalculatePerformance(Performance performance)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
