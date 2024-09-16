e7efac574d442a5046c8be3fcd60790f6d268027
Posccis <lkzq33@gmail.com>
2024-09-16 14:09:16 -0300

    new(teste/victor-hugo): Desenvolvimento da interface de calculo das performances para definir o contrato que as classes devem ter

---
+++ TheatricalPlayersRefactoringKata/Application/Interfaces/ITypeCalc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheatricalPlayersRefactoringKata.Domain.Domains;
+
+namespace TheatricalPlayersRefactoringKata.Application.Interfaces
+{
+    public interface ITypeCalc
+    {
+        string CalculatePerformance(Performance performance);
+    }
+}
